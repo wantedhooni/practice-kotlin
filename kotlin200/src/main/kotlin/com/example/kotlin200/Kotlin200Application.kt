efd1498cc10f79f58a92694d1f01baa2f7c26585
Revy <wantedhooni@gmail.com>
2023-10-12 14:10:36 +0900

    코틀린 200제 셋팅

---
+++ kotlin200/src/main/kotlin/com/example/kotlin200/Kotlin200Application.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin200
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlin200Application
+
+fun main(args: Array<String>) {
+    runApplication<Kotlin200Application>(*args)
+}
---
+++ kotlin200/src/test/kotlin/com/example/kotlin200/Kotlin200ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin200
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kotlin200ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
